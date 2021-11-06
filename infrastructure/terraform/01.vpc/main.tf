1f622ad7284b3098652ea4213c3e010cbc5285a9
Morten Hjorth Fæster <faester@gmail.com>
2021-11-06 07:26:56 +0000

    One vpc - subnets in both AZs

---
+++ infrastructure/terraform/01.vpc/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = "~> 0.13.6"
+  backend "s3" {
+    encrypt        = true
+    bucket         = "mfaester-booking-state"
+    dynamodb_table = "mfaester-booking-state-locks"
+    key            = "booking/01.vpc/terraform.tfstate"
+    region         = "eu-west-1"
+    profile        = "mfaester"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  version = "~> 3.0"
+  region  = "eu-west-1"
+  profile = "mfaester"
+  default_tags {
+    tags = {
+      Source = path.cwd
+    }
+  }
+}
+
+resource "aws_subnet" "back-a" {
+  vpc_id            = aws_vpc.front.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "Back"
+  }
+}
+
+resource "aws_subnet" "back-b" {
+  vpc_id            = aws_vpc.front.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-1b"
+
+  tags = {
+    Name = "Back"
+  }
+}
+
+resource "aws_vpc" "front" {
+  cidr_block = "10.0.0.0/23"
+}
