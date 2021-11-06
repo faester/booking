ca65a205d4079a7826add3cadc549d4a9cd5c2bb
Morten Hjorth Fæster <faester@gmail.com>
2021-11-06 13:34:26 +0000

    cluster created

--- infrastructure/terraform/environment/02.cluster/main.tf
+++ infrastructure/terraform/environment/02.cluster/main.tf
@@ -41,7 +41,7 @@ data aws_subnet subnet_b {
 }
 
 data aws_ssm_parameter ecs_ami {
-	name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
 }
 
 resource aws_launch_template booking {
@@ -81,6 +81,37 @@ resource aws_autoscaling_group booking {
   }
 
   lifecycle {
-	ignore_changes = [desired_capacity]
+    ignore_changes = [desired_capacity]
+  }
+}
+
+resource aws_ecs_capacity_provider booking {
+  name = "booking-main-capacity-procider"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.booking.arn
+    managed_termination_protection = "DISABLED"
+
+    managed_scaling {
+      maximum_scaling_step_size = 1000
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 100
+    }
+  }
+}
+
+resource "aws_iam_service_linked_role" "ecs" {
+  aws_service_name = "ecs.amazonaws.com"
+}
+
+resource aws_ecs_cluster booking {
+  name = "booking-main"
+
+  capacity_providers = [aws_ecs_capacity_provider.booking.name]
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
   }
 }
