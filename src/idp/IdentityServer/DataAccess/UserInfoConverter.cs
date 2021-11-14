64a8258fabd42e87e8e9ece75b20f3d3d15c4396
Morten Fæster <morten.faester@jppol.dk>
2021-11-14 21:25:01 +0100

    User info contains roles.

--- src/idp/IdentityServer/DataAccess/UserInfoConverter.cs
+++ src/idp/IdentityServer/DataAccess/UserInfoConverter.cs
@@ -7,30 +7,33 @@ namespace IdentityServer.DataAccess
 {
     public class UserInfoConverter : BaseConverter<UserInformation>, ISimpleDbConverter<UserInformation>
     {
-        private const string ADDRESS_FIELD = "address";
-        private const string PHONE_FIELD = "phone";
-        private const string EMAIL_FIELD = "email";
-
         public UserInfoConverter() 
             : base(x => new UserInformation{SubjectId = x}, ui => ui.SubjectId)
         {
-            AddFieldMapping(ADDRESS_FIELD, 
+            AddFieldMapping("address", 
                 (x, v) => x.Address = v, 
                 x => x.Address,
                 x => x.Address);
-            AddFieldMapping(PHONE_FIELD, 
+            AddFieldMapping("phone", 
                 (x, v) => x.Phone = v,
                 x => x.Phone,
                 x => x.Phone);
-            AddFieldMapping(EMAIL_FIELD, 
+            AddFieldMapping("email",
                 (x, v) => x.Email = v,
                 x => x.Email,
                 x => x.Email);
-        }
-
-        public string GetSimpleDbFieldNameFor(Expression<Func<UserInformation, object>> dtoField)
-        {
-            return base.GetSimpleDbName(dtoField);
+            AddFieldMapping("is_administrator",
+                (x, v) => x.IsAdministrator = v == "true",
+                x => x.IsAdministrator ? "true" : "false",
+                x => x.IsAdministrator);
+            AddFieldMapping("is_speaker",
+                (x, v) => x.IsSpeaker = v == "true",
+                x => x.IsSpeaker ? "true" : "false",
+                x => x.IsSpeaker);
+            AddFieldMapping("is_validated_user",
+                (x, v) => x.IsValidatedUser = v == "true",
+                x => x.IsValidatedUser ? "true" : "false",
+                x => x.IsValidatedUser);
         }
     }
 }
