d81d586135f620205f91b75307a06642843487da
Morten Fæster <morten.faester@jppol.dk>
2021-11-15 01:26:02 +0100

    Stub for convention website

---
+++ src/convention-website/Startup.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+namespace convention_website
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddAuthentication(opts =>
+                {
+                    opts.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    opts.DefaultChallengeScheme = "oidc";
+                }).AddCookie()
+                .AddOpenIdConnect("oidc", opts =>
+                {
+                    opts.MetadataAddress = "https://identity-server.mfaester.dk/.well-known/openid-configuration";
+                    opts.AuthenticationMethod = OpenIdConnectRedirectBehavior.FormPost;
+                    opts.ClientId = "consent-website";
+                    opts.Scope.Clear();
+                    opts.Scope.Add(JwtRegisteredClaimNames.Email);
+                    opts.Scope.Add(JwtRegisteredClaimNames.GivenName);
+                    opts.Scope.Add(JwtRegisteredClaimNames.FamilyName);
+                    opts.Scope.Add("phone");
+                    opts.Scope.Add("role");
+                    opts.Scope.Add("openid");
+                    opts.UsePkce = true;
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
