4974e175ebc740f04430d7b2797f62cdde7b353d
Morten Fæster <morten.faester@jppol.dk>
2021-11-15 03:08:18 +0100

    UseAuthentication

--- src/convention-website/Startup.cs
+++ src/convention-website/Startup.cs
@@ -62,6 +62,7 @@ namespace convention_website
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
