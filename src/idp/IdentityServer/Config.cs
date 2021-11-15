4d07c5624f8677cd63af4afd432c412c144fde33
Morten Fæster <morten.faester@jppol.dk>
2021-11-15 01:41:31 +0100

    Client setup. Adding openid scopes

--- src/idp/IdentityServer/Config.cs
+++ src/idp/IdentityServer/Config.cs
@@ -4,22 +4,25 @@
 
 using IdentityServer4.Models;
 using System.Collections.Generic;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace IdentityServer
 {
     public static class Config
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
-            new IdentityResource[]
+            new[]
             {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Address(),
                 new IdentityResources.Phone(),
                 new IdentityResources.Email(),
+                new IdentityResources.OpenId(),
+                new IdentityResource("role","Roles and user status", new []{"role"}),
             };
 
         public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
+            new[]
             {
                 new ApiScope("convention-admin", "Administrate conventions"),
                 new ApiScope("convention-read", "Read convention data"),
@@ -38,6 +41,24 @@ namespace IdentityServer
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     AccessTokenLifetime = 3600,
                     AuthorizationCodeLifetime = 300,
+                },
+                new Client {
+                    ClientId = "convention-website",
+                    ClientName = "convention-website",
+                    ClientSecrets = new Secret[]{},
+                    AllowedScopes =
+                    {
+                        "openid", "name", "address", "phone", JwtRegisteredClaimNames.Email, JwtRegisteredClaimNames.GivenName, JwtRegisteredClaimNames.FamilyName
+                    },
+                    AccessTokenType = AccessTokenType.Jwt,
+                    AllowAccessTokensViaBrowser = true,
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AccessTokenLifetime = 3600,
+                    AuthorizationCodeLifetime = 300,
+                    RedirectUris = new []{
+                        "https://consent-website.mfaester.dk/signin-oidc", 
+                        "https://localhost:44316/signin-oidc",
+                    }
                 }
             };
     }
